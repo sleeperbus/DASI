9612d7ac6a9c8c35a85aebf658de3c5704589da2
Hojoon Ji <sleeperbus@gmail.com>
2015-10-08 11:22:47 +0000

    ...

--- lab3.R
+++ lab3.R
@@ -57,3 +57,34 @@ for (i in 1:5000) {
 }
 hist(sample_means50)
 summary(sample_means50)
+
+# 1
+population <- ames$Gr.Liv.Area
+samp <- sample(population, 60)
+hist(samp)
+boxplot(samp)
+
+# 2, 3
+sample_mean <- mean(samp)
+se <- sd(samp) / sqrt(60)
+lower <- sample_mean - 1.96 * se
+upper <- sample_mean + 1.96 * se
+c(lower, upper)
+
+# 4
+mean(population)
+
+# 5
+samp_mean <- rep(NA, 50)
+samp_sd <- rep(NA, 50)
+n <- 60
+for (i in 1:50) {
+  samp <- sample(population, n)
+  samp_mean[i] <- mean(samp)
+  samp_sd[i] <- sd(samp)
+}
+
+lower <- samp_mean - 1.96 * samp_sd
+upper <- samp_mean + 1.96 * samp_sd
+c(lower[1], upper[1])
+plot_ci(lower, upper, mean(population))
